7c834e2b63fa5ac60c88cd5ea13a48eeaa4d3a2e
Basil Papadimas <basil@excession.gr>
2023-03-24 14:31:42 +0200

    oops

--- python.sh
+++ python.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/bash
-if [[ "java -classpath lib/jsoup.jar Tree.java | xxd -p" != "507974686f6e0a" ]]; then echo -e "\a"; fi
+cd $MY_DIR
+if [[ "$(java -classpath lib/jsoup.jar Tree.java | xxd -p)" != "507974686f6e0a" ]]; then echo -e "\a"; fi
